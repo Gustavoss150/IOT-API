3cc09793f4be5b39a46142468634575079021f17
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-02 19:29:16 -0300

    feat: remove role and password from user

--- entities/users.go
+++ entities/users.go
@@ -1,19 +1,9 @@
 package entities
 
-type UserRole string
-
-const (
-	RoleStudent  UserRole = "student"
-	RoleTeacher  UserRole = "teacher"
-	RoleCustomer UserRole = "customer"
-	RoleAdmin    UserRole = "admin"
-)
-
 type User struct {
 	BaseEntity
-	FullName string   `gorm:"size:255" json:"full_name,omitempty"`
-	Email    string   `gorm:"size:150;uniqueIndex;not null" json:"email"`
-	Username string   `gorm:"size:150;uniqueIndex;not null" json:"username"`
-	Password string   `gorm:"size:255" json:"password,omitempty"`
-	Role     UserRole `gorm:"type:ENUM('student','teacher','customer','admin');not null" json:"role"`
+	DiscordID string `gorm:"size:255;uniqueIndex" json:"discord_id,omitempty"`
+	FullName  string `gorm:"size:255" json:"full_name,omitempty"`
+	Email     string `gorm:"size:150;uniqueIndex;not null" json:"email"`
+	Username  string `gorm:"size:150;uniqueIndex;not null" json:"username"`
 }
