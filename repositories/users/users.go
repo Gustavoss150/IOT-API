ecd7d30507ac94307a1771bcf6f4ee654eb16c77
Gustavoss150 <gustavoss1@outlook.com>
2025-10-09 20:41:40 -0300

    repositorio de usuarios

--- repositories/users/users.go
+++ repositories/users/users.go
@@ -1,1 +1,49 @@
 package usersRepo
+
+import (
+	"api/config"
+	"api/entities"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type usersRepository struct {
+	DB *gorm.DB
+}
+
+func InitUsersRepository() (UsersRepository, error) {
+	db := config.DB
+	if db == nil {
+		return nil, errors.New("failed to connect to database")
+	}
+	return &usersRepository{DB: db}, nil
+}
+
+func (r *usersRepository) Save(user *entities.User) error {
+	return r.DB.Save(user).Error
+}
+
+func (r *usersRepository) GetByID(userID string) (*entities.User, error) {
+	var user entities.User
+	if err := r.DB.Where("id = ?", userID).First(&user).Error; err != nil {
+		return nil, errors.New("usuário não encontrado")
+	}
+	return &user, nil
+}
+
+func (r *usersRepository) GetByDiscordID(discordID string) (*entities.User, error) {
+	var user entities.User
+	if err := r.DB.Where("discord_id = ?", discordID).First(&user).Error; err != nil {
+		return nil, errors.New("usuário não encontrado")
+	}
+	return &user, nil
+}
+
+func (r *usersRepository) GetAll() ([]*entities.User, error) {
+	var users []*entities.User
+	if err := r.DB.Order("created_at DESC").Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
--- repositories/users/users_interface.go
+++ repositories/users/users_interface.go
@@ -1,1 +1,10 @@
 package usersRepo
+
+import "api/entities"
+
+type UsersRepository interface {
+	Save(user *entities.User) error
+	GetByID(userID string) (*entities.User, error)
+	GetByDiscordID(discordID string) (*entities.User, error)
+	GetAll() ([]*entities.User, error)
+}
