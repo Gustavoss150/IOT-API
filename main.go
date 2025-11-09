25614e471f8cc408426f23eba76d81c39ed7bc55
Gustavoss150 <gustavoss1@outlook.com>
2025-11-09 10:03:29 -0300

    atualização DDL

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"api/config"
 	"api/migrations"
 	"api/server"
-	"api/services"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,7 +17,7 @@ func main() {
 
 	server.InitRoutes(r)
 
-	go services.StartMQTT()
+	// go services.StartMQTT()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "API is working"})
